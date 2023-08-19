a01b7658ea052cbf37463c483fbe4b4ef8e916bc
Joker <40009719+pic123123@users.noreply.github.com>
2023-08-19 10:43:52 +0900

    기본끝

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,76 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
+}
+
+class MovieResponse {
+  final int page;
+  final List<Movie> results;
+
+  MovieResponse({required this.page, required this.results});
+
+  factory MovieResponse.fromJson(Map<String, dynamic> json) {
+    return MovieResponse(
+        page: json['page'],
+        results: List<Movie>.from(
+            json['results'].map((item) => Movie.fromJson(item))));
+  }
+}
+
+class Movie {
+  final bool adult;
+  final String backdropPath;
+  final List<int> genreIds;
+  final int id;
+  final String originalLanguage;
+  final String originalTitle;
+  final String overview;
+  final double popularity;
+  final String posterPath;
+  final String releaseDate;
+  final String title;
+  final bool video;
+  final double voteAverage;
+  final int voteCount;
+
+  Movie({
+    required this.adult,
+    required this.backdropPath,
+    required this.genreIds,
+    required this.id,
+    required this.originalLanguage,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    required this.posterPath,
+    required this.releaseDate,
+    required this.title,
+    required this.video,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+        adult: json['adult'],
+        backdropPath: json['backdrop_path'],
+        genreIds: List<int>.from(json['genre_ids']),
+        id: json['id'],
+        originalLanguage: json['original_language'],
+        originalTitle: json['original_title'],
+        overview: json['overview'],
+        popularity: json['popularity'].toDouble(),
+        posterPath: json['poster_path'],
+        releaseDate: json['release_date'],
+        title: json['title'],
+        video: json['video'],
+        voteAverage: json['vote_average'].toDouble(),
+        voteCount: json['vote_count']);
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -10,36 +79,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Movie',
+      home: MyHomePage(title: 'Movie'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +96,200 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  //데이터 가져와서 json에 담음
+  Future<MovieResponse> fetchPopularMovies() async {
+    final response = await http
+        .get(Uri.parse('https://movies-api.nomadcoders.workers.dev/popular'));
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    if (response.statusCode == 200) {
+      return MovieResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to fetch movies');
+    }
+  }
+
+  Future<MovieResponse> fetchNowPlayingMovies() async {
+    final response = await http.get(
+        Uri.parse('https://movies-api.nomadcoders.workers.dev/now-playing'));
+
+    if (response.statusCode == 200) {
+      return MovieResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to fetch movies');
+    }
+  }
+
+  Future<MovieResponse> fetchComingSoonMovies() async {
+    final response = await http.get(
+        Uri.parse('https://movies-api.nomadcoders.workers.dev/coming-soon'));
+
+    if (response.statusCode == 200) {
+      return MovieResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to fetch movies');
+    }
+  }
+
+  late Future<MovieResponse> futurePopularMovies;
+  late Future<MovieResponse> futureNowPlayingMovies;
+  late Future<MovieResponse> futureComingSoonMovies;
+
+  @override
+  void initState() {
+    super.initState();
+    futurePopularMovies = fetchPopularMovies();
+    futureNowPlayingMovies = fetchNowPlayingMovies();
+    futureComingSoonMovies = fetchComingSoonMovies();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        backgroundColor: Colors.grey[900],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            // 이 부분 추가
+            padding: const EdgeInsets.all(8.0),
+            child: const Text(
+              "Popular Movies",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Expanded(
+            child: Center(
+              child: FutureBuilder<MovieResponse>(
+                future: futurePopularMovies,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: snapshot.data!.results.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.network(
+                            'https://image.tmdb.org/t/p/w500${snapshot.data!.results[index].posterPath}',
+                          ),
+                        );
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
+
+                  return const CircularProgressIndicator();
+                },
+              ),
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Now in cinemas",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Expanded(
+                  child: FutureBuilder<MovieResponse>(
+                    future: futureNowPlayingMovies,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: snapshot.data!.results.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: SizedBox(
+                                width: 100, // 이미지와 텍스트의 최대 너비를 설정합니다.
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    AspectRatio(
+                                      aspectRatio: 0.7,
+                                      child: ClipRRect(
+                                        borderRadius:
+                                            BorderRadius.circular(8.0),
+                                        child: Image.network(
+                                          'https://image.tmdb.org/t/p/w500${snapshot.data!.results[index].posterPath}',
+                                          fit: BoxFit.fitHeight,
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                        height: 8.0), // 제목과 이미지 사이의 간격을 추가
+                                    Flexible(
+                                      child: Text(
+                                        snapshot.data!.results[index].title,
+                                        style: const TextStyle(fontSize: 16),
+                                        maxLines: 2,
+                                        overflow: TextOverflow.ellipsis,
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text("${snapshot.error}");
+                      }
+
+                      return const CircularProgressIndicator();
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            // 이 부분 추가
+            padding: const EdgeInsets.all(8.0),
+            child: const Text(
+              "Comming Soon",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: FutureBuilder<MovieResponse>(
+                future: futureComingSoonMovies,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: snapshot.data!.results.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.network(
+                            'https://image.tmdb.org/t/p/w500${snapshot.data!.results[index].posterPath}',
+                          ),
+                        );
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
+
+                  return const CircularProgressIndicator();
+                },
+              ),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
